69741bd464abc23b4868d888b8e047c944c78922
João Paulo <joaopaulodelanapicoli2@gmail.com>
2026-04-09 10:51:32 -0300

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int ano,v1;
-        System.out.println("Digite o ano em que vocé nasceu e falaremos se voce e maior de idade ou não");
+        int ano, v1;
+        System.out.println("Digite o ano em que vocé nasceu e o ano atual falaremos se voce e maior de idade ou não");
         ano = scan.nextInt();
-        v1 = 2026 - ano;
-        if (v1 >= 18){
+        v1 = scan.nextInt();
+        if (v1 - ano >= 18) {
             System.out.println("Vocé é maior de idade");
-        }else {
+        } else {
             System.out.println("Vocé e menor de idade");
         }
     }
